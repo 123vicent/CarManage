533de43b505efcbe6df2169acf90133f4adf9183
seizerccc <907839891@qq.com>
2018-07-03 16:01:07 +0800

    dbConnector & Car class

--- src/main/java/basic/Car.java
+++ src/main/java/basic/Car.java
@@ -1,4 +1,44 @@
 package basic;
 
 public class Car {
+    //editror
+    //dai cheng zhi
+    //07-03 15:46
+
+    private String type;
+    private String name;
+    private String color;
+    private double price;
+
+    public String getType(){
+        return type;
+    }
+
+    public void setType(String type){
+        this.type = type;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
