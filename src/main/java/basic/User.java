6bbd50d1e24e83d771c6419bc2bb7e8076cfbfa5
seizerccc <907839891@qq.com>
2018-07-03 16:26:12 +0800

    basic attributes of user(shop user)

--- src/main/java/basic/User.java
+++ src/main/java/basic/User.java
@@ -1,4 +1,44 @@
 package basic;
 
 public class User {
+    //editor
+    //dai cheng zhi
+    //07-03 16:16
+    private String name;
+    private String phone;
+    private String address;
+    private String manager;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
